36ad751cfd802cdb5b5a51681e904ec45493b0e2
Wilcabo <wilcabo2011@hotmail.com>
2025-10-06 17:16:20 -0500

    comit

--- src/app/repositorio-conocimiento-component/repositorio-conocimiento-component.ts
+++ src/app/repositorio-conocimiento-component/repositorio-conocimiento-component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-repositorio-conocimiento-component',
   imports: [],
+  standalone: true,
   templateUrl: './repositorio-conocimiento-component.html',
   styleUrl: './repositorio-conocimiento-component.css'
 })
